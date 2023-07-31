5912092532d100b149dafd4321bde6ff36be6cd0
Marcelo Catalan <marcelocatalan99@hotmail.com>
2023-07-31 12:16:35 -0400

    Segunda pre entrega

--- index.js
+++ index.js
@@ -1,48 +1,96 @@
-let producto1 = 9990;
-let producto2 = 6800;
-let producto3 = 5700;
+const productos = [
+  { nombre: 'Lomo Vetado', precio: 9900 },
+  { nombre: 'Cerdo Marinado', precio: 7000 },
+  { nombre: 'Chuleta Parrillera Cerdo', precio: 4500 },
+  { nombre: 'Chunchules', precio: 3000 },
+  { nombre: 'Entraña', precio: 8990 },
+  { nombre: 'Truto Pollo Grande', precio: 3250 },
+];
 
-alert("PROMOCION ASADO FAMILIAR!!! Elige la cantidad de los productos que tenemos disponibles para la promocion y sorprendete con el TOTAL!!");
+// Esta funcion muestra el menu de opciones disponibles
+function obtenerOpcionMenu() {
+  return parseInt(prompt(
+    "---- CARNICERIA METROPOLITANA SANTIAGO ----\n"
+    + "1 - Ver todos los productos\n"
+    + "2 - Comprar productos\n"
+    + "3 - Filtrar por precio\n"
+    + "4 - Salir"
+  ));
+}
 
-let cantidadProducto1 = 0;
-let cantidadProducto2 = 0;
-let cantidadProducto3 = 0;
+// Esta funcion muestra los productos disponibles en catalogo
+function catalogoProductos() {
+  let mensajeProductos = "---- CATALOGO DE PRODUCTOS ----\n";
+  productos.forEach((producto, index) => {
+    mensajeProductos += `${index + 1}. ${producto.nombre} - $${producto.precio} x kg\n`;
+  });
+  alert(mensajeProductos);
+}
 
+// Esta funcion permite comprar productos por kilo y al final suma el total de la compra
+function comprarProductos() {
+  let total = 0;
 
-while (cantidadProducto1 < 1 || cantidadProducto1 > 5) {
+  function comprarProducto(index) {
+    if (index >= productos.length) {
+      alert(`Total a pagar: $${total}\nGracias por su compra`);
+      return;
+    }
 
-  cantidadProducto1 = parseInt(prompt("Cuantos kg de Lomo Vetado desea comprar (máximo 5 kg):"));
-  if (cantidadProducto1 < 1 || cantidadProducto1 > 5) {
-    alert("Ingrese una cantidad válida para el producto 1");
+    const producto = productos[index];
+    const cantidad = prompt(`¿Cuántos kg de ${producto.nombre} desea llevar? (Si quiere llevar este producto coloque 0)`);
+
+    // Aca se calcula el total de la compra
+    total += parseInt(cantidad) * producto.precio;
+
+    comprarProducto(index + 1);
+  }
+
+  comprarProducto(0);
+}
+
+// Esta funcion ocupa el metodo de busqueda filter que permite filtrar una array por medio de una condicion que en este caso es mediante el precio de estos productos.
+// Y para buscar los productos en la array ocupamos un forEach para iterar cada objeto.
+function filtrarProductoPrecio() {
+  const precioLimite = parseFloat(prompt('Ingrese el precio máximo para filtrar:'));
+
+  let mensajeProductosFiltrados = "PRODUCTOS ENCONTRADOS\n";
+  const productosFiltrados = productos.filter(producto => producto.precio <= precioLimite);
+
+  if (productosFiltrados.length > 0) {
+    productosFiltrados.forEach((producto, index) => {
+      mensajeProductosFiltrados += `${index + 1}. ${producto.nombre} - Precio: $${producto.precio}\n`;
+    });
+    alert(mensajeProductosFiltrados);
+  } else {
+    alert("No se encontraron productos por debajo del precio estimado");
   }
 }
+// Esta funcion muestra las opciones disponibles y es la funcion principal que contiene las demas funciones y opciones.
+function carniceria() {
+  let opcion;
+  do {
+    opcion = obtenerOpcionMenu();
 
-while (cantidadProducto2 < 1 || cantidadProducto2 > 3) {
-
-  cantidadProducto2 = parseInt(prompt("Cuantos paquetes de 8 chorizos desea comprar (maximo 3)"));
-  if (cantidadProducto2 < 1 || cantidadProducto2 > 3) {
-    alert("Ingrese una cantidad válida para el producto 2");
-  }
+    switch (opcion) {
+      case 1:
+        catalogoProductos();
+        break;
+      case 2:
+        comprarProductos();
+        break;
+      case 3:
+        filtrarProductoPrecio();
+        break;
+      case 4:
+        alert("Gracias por visitarnos!");
+        break;
+      default:
+        alert("Opcion invalida, ingrese una de las que muestra el menu.");
+        break;
+    }
+  } while (opcion !== 4);
 }
 
-while (cantidadProducto3 < 1 || cantidadProducto3 > 4) {
-    
-  cantidadProducto3 = parseInt(prompt("Cuantos kg de Pulpa Cerdo Marinada desea comprar (máximo 4 kg):"));
-  if (cantidadProducto3 < 1 || cantidadProducto3 > 4) {
-    alert("Ingrese una cantidad válida para el producto 3");
-  }
-}
-
-let costoTotal = 0;
-
-function sumaTotalProductos() {
-  let costoTotal1 = cantidadProducto1 * producto1;
-  let costoTotal2 = cantidadProducto2 * producto2;
-  let costoTotal3 = cantidadProducto3 * producto3;
-
-  let valorTotal = costoTotal1 + costoTotal2 + costoTotal3;
-  return valorTotal;
-}
-
-costoTotal = sumaTotalProductos();
-alert("El costo total de la compra es: $" + costoTotal);
+// Aca llamamos a la funcion principal para arrancar el programa
+carniceria();
